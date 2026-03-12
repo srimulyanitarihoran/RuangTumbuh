d715bc0a5f7bc5281a6275ce7c29199ece0ca388
adyvka31 <rafifdyvka07@gmail.com>
2026-03-12 23:48:59 +0700

    feat: add react lenis for smoot scrolling

--- rutu-frontend/src/App.jsx
+++ rutu-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ReactLenis } from 'lenis/react'
 import HomePage from "./pages/HomePage/HomePage";
 import Login from "./pages/LoginPage/LoginPage";
 import Register from "./pages/RegisterPage/RegisterPage";
@@ -7,13 +8,16 @@ import CustomCursor from "./components/CustomCursor/CustomCursor";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <CustomCursor />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    // Gunakan lerp 0.08 untuk efek mulus yang natural. Jangan gunakan parameter 'duration'.
+    <ReactLenis root>
+      <BrowserRouter>
+        <CustomCursor />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </ReactLenis>
   );
 }
