2fd163ae34ce59061e1e4d6966ca841334b46204
adyvka31 <rafifdyvka07@gmail.com>
2026-04-08 09:15:14 +0700

    fix in server.js

--- rutu-backend/server.js
+++ rutu-backend/server.js
@@ -4,7 +4,7 @@ const app = require("./src/app");
 const PORT = process.env.PORT || 5001;
 
 const server = app.listen(PORT, () => {
-  console.log(`🚀 Server berjalan optimal di http://localhost:${PORT}`);
+  console.log(`🚀 Server berjalan optimal pada Port: ${PORT}`);
 });
 
 server.on("error", (err) => {
